8c6c70c59af0e5d0f716649ce2e0ca525d153403
Amit Upadhyay <ankur007upadhyay@gmail.com>
2017-10-13 18:03:48 +0530

    A Simple BrainF**k interpreter in C#

---
+++ Brainf__k.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brainf__k_Interpreter
+{
+    class Brainf__k
+    {
+        private static string VERSION = "v0.0.1";
+        private static readonly int BUFFERSIZE = ushort.MaxValue;
+        private int[] buffer = new int[BUFFERSIZE];
+        private int pointer { get; set; }
+        private int get_value_at_pointer()
+        {
+            return this.buffer[this.pointer];
+        }
+        private void clear()
+        {
+            Array.Clear(this.buffer, 0, this.buffer.Length);
+        }
+        private void increment_pointer()
+        {
+            this.pointer++;
+            if (this.pointer >= BUFFERSIZE)
+            {
+                this.pointer = 0;
+            }
+
+        }
+        private void decrement_pointer()
+        {
+            this.pointer--;
+            if (this.pointer < 0)
+            {
+                this.pointer = BUFFERSIZE - 1;
+            }
+
+        }
+        private void increment_value()
+        {
+            this.buffer[this.pointer]++;
+        }
+        private void decrement_value()
+        {
+            this.buffer[this.pointer]--;
+        }
+        private void putchar()
+        {
+            Console.Write((char)this.buffer[this.pointer]);
+        }
+        private void getchar()
+        {
+            ConsoleKeyInfo key = Console.ReadKey(true);
+            this.buffer[this.pointer] = (int)key.KeyChar;
+        }
+        
+        public Brainf__k()
+        {
+            this.pointer = 0;
+            this.clear();
+        }
+        public static void print_version()
+        {
+            Console.WriteLine("BrainFuck Interpreter " + VERSION);
+            Console.WriteLine("By Amit Upadhyay");
+        }
+        public static void print_help()
+        {
+
+            Console.WriteLine("BrainFuck Interpreter " + VERSION);
+            Console.WriteLine("By Amit Upadhyay");
+            Console.WriteLine("Parameter: -h: Help");
+            Console.WriteLine("Parameter: -k: Input via keyboard");
+            Console.WriteLine("Parameter: -v: Version");
+            Console.WriteLine("Parameter: FileName: *.bf program");
+
+        }
+        public void interpret(string program)
+        {
+            int bound = program.Length;
+            
+            int iterator = 0;
+            while(iterator < bound)
+            {
+                switch(program[iterator])
+                {
+                    case '>':
+                        {
+                            
+                            increment_pointer();
+                            
+                            break;
+                        }
+                    case '<':
+                        {
+                            decrement_pointer();
+                            break;
+                        }
+                    case '+':
+                        {
+                            increment_value();
+                            break;
+                        }
+                    case '-':
+                        {
+                            decrement_value();
+                            break;
+                        }
+                    case '.':
+                        {
+                            putchar();
+                            break;
+                        }
+                    case ',':
+                        {
+                            getchar();
+                            break;
+                        }
+                    case '[':
+                        {
+                            if(get_value_at_pointer() == 0)
+                            {
+                                int loopnumber = 1;
+                                while(loopnumber > 0)
+                                {
+                                    iterator++;
+                                    char current = program[iterator];
+                                    if(current == '[')
+                                    {
+                                        loopnumber++;
+                                    }
+                                    else if(current == ']')
+                                    {
+                                        loopnumber--;
+                                    }
+                                }
+                            }
+                            break;
+                        }
+                    case ']':
+                        {
+                            int loopnumber = 1;
+                            while (loopnumber > 0)
+                                {
+                                    iterator--;
+                                    char current = program[iterator];
+                                    if (current == '[')
+                                    {
+                                        loopnumber--;
+                                    }
+                                    else if (current == ']')
+                                    {
+                                        loopnumber++;
+                                    }
+                                }
+                            
+                            iterator--;
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+
+                }
+                iterator++;
+            }
+        }
+        
+    }
+}
---
+++ Main_Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brainf__k_Interpreter
+{
+    class Main_Program
+    {
+        static void Main(string[] args)
+        {
+            bool error = false;
+            if(args.Length == 0)
+            {
+                Brainf__k.print_help();
+            }
+            else if(args.Length == 1)
+            {
+                Brainf__k bf_interpreter = new Brainf__k();
+                string c_arg = args[0];
+                
+                if(c_arg[0] == '-' && c_arg.Length == 2)
+                {
+                    switch(c_arg[1])
+                    {
+                        case 'h':
+                            {
+                                Brainf__k.print_help();
+                                break;
+                            }
+                        case 'v':
+                            {
+                                Brainf__k.print_version();
+                                break;
+                            }
+                        case 'k':
+                            {
+                                Console.WriteLine("Enter Code Below:");
+                                string src = Console.In.ReadToEnd();
+                                bf_interpreter.interpret(src);
+                                break;
+                            }
+                        default:
+                            {
+                                error = true;
+                                break;
+                            }
+
+                    }
+                }
+                else if (c_arg[0] == '-' && c_arg.Length != 2)
+                {
+                    error = true;
+                }
+                else
+                {
+                    if (File.Exists(c_arg))
+                    {
+                        bf_interpreter.interpret(File.ReadAllText(c_arg));
+                    }
+                    else
+                    {
+                        Console.WriteLine("File Open Error: " + c_arg);
+                        error = true;
+                    }
+
+                }
+            }
+            else
+            {
+                error = true;
+            }
+            if(error)
+            {
+                Console.WriteLine("Bad Command");
+                Brainf__k.print_help();
+            }
+        }
+    }
+}
